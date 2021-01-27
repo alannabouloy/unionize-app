1e2d960301d2d7c9c10572ac25b36e3fac332fc0
Alanna Bouloy <lanihope@gmail.com>
2021-01-27 18:13:36 -0500

    added components for FAQ page

---
+++ src/components/QuestionTab/QuestionTab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './QuestionTab.css'
+
+export default function QuestionTab(props){
+    return(
+        <div className='question'>
+            <h3>{props.question}</h3>
+            <p>{props.answer}</p>
+        </div>
+    )
+}
--- src/routes/FAQPage/FAQPage.js
+++ src/routes/FAQPage/FAQPage.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import QuestionTab from '../../components/QuestionTab/QuestionTab'
+import SectionHeading from '../../components/SectionHeading/SectionHeading'
 import './FAQPage.css'
 
 export default class FAQPage extends Component {
     render(){
         return(
             <section className='faq-page'>
-                
+               <SectionHeading className='faq-heading' text='Frequently Asked Questions'/>
+                <div className='questions-block'>
+                    <div className='question'>
+                        <QuestionTab question='Question 1' answer='This is an answer to the question'/>
+                    </div>
+                </div>
             </section>
         )
     }
