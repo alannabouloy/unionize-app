bf46420ff9e66b0bf74955bfd6183021b45a81a8
Alanna Bouloy <lanihope@gmail.com>
2021-01-27 17:50:56 -0500

    created About Page components

---
+++ src/components/SectionHeading/SectionHeading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './SectionHeading.css'
+
+export default function SectionHeading(props){
+    return(
+        <div  className={`section-heading ${props.className}`}>
+            <h2>{props.text}</h2>
+        </div>
+    )
+}
--- src/routes/AboutPage/AboutPage.js
+++ src/routes/AboutPage/AboutPage.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import SectionHeading from '../../components/SectionHeading/SectionHeading'
 import './AboutPage.css'
 
 export default class AboutPage extends Component {
     render(){
         return(
             <section className='about-page'>
-                
+                <SectionHeading className='about-heading' text='What is Unionize?'/>
+                <div className='about-msg'>
+                    <p>
+                        Unionize is an app designed to put you in touch with unions that server your industry.
+                        We believe that every worker has the right to unionize and that no employer
+                        should be given absolute power over their employees, which is why unionize makes it easy
+                        to find and join a union.
+                    </p>
+                </div>
             </section>
         )
     }
