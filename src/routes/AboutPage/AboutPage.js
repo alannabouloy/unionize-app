0588239c0c5667d79d009909c86a46bd35cf20a0
Alanna Bouloy <lanihope@gmail.com>
2021-01-30 21:27:33 -0500

    About Page styled

--- src/routes/AboutPage/AboutPage.js
+++ src/routes/AboutPage/AboutPage.js
@@ -9,7 +9,7 @@ export default class AboutPage extends Component {
                 <SectionHeading className='about-heading' text='What is Unionize?'/>
                 <div className='about-msg'>
                     <p>
-                        Unionize is an app designed to put you in touch with unions that server your industry.
+                        Unionize is an app designed to put you in touch with unions that serve your industry.
                         We believe that every worker has the right to unionize and that no employer
                         should be given absolute power over their employees, which is why unionize makes it easy
                         to find and join a union.
