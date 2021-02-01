b2bcaa1946ff063e89a78bad314a0195b39eab40
Alanna Bouloy <lanihope@gmail.com>
2021-02-01 01:47:25 -0500

    predeploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
-    API_TOKEN: 'test-token'
+    API_ENDPOINT: 'https://dry-reaches-87351.herokuapp.com/api',
+    API_TOKEN: process.env.REACT_APP_API_TOKEN
 }
